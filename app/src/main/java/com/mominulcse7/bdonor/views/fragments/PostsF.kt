7fcdb40161b4c3bc13688cb610e688c6dc6c958a
Mominul <mominsjob@gmail.com>
2022-09-01 16:26:32 +0600

    nav_graph update

--- app/src/main/java/com/mominulcse7/bdonor/views/fragments/PostsF.kt
+++ app/src/main/java/com/mominulcse7/bdonor/views/fragments/PostsF.kt
@@ -6,7 +6,6 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.viewModels
-import com.mominulcse7.bdonor.R
 import com.mominulcse7.bdonor.databinding.FragmentPostsBinding
 import com.mominulcse7.bdonor.viewModel.HomeViewModel
 
