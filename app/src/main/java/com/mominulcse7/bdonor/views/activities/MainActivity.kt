7548f0afeece7c67e2e40b1ab75dc6f7b2a54def
Mominul <mominsjob@gmail.com>
2022-09-01 18:21:41 +0600

    icon color change

--- app/src/main/java/com/mominulcse7/bdonor/views/activities/MainActivity.kt
+++ app/src/main/java/com/mominulcse7/bdonor/views/activities/MainActivity.kt
@@ -1,13 +1,17 @@
 package com.mominulcse7.bdonor.views.activities
 
 import android.os.Bundle
+import android.view.MenuItem
 import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.findNavController
 import com.google.android.material.bottomnavigation.BottomNavigationView
+import com.google.android.material.navigation.NavigationBarView
+import com.mominulcse7.bdonor.R
 import com.mominulcse7.bdonor.databinding.ActivityMainBinding
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), NavigationBarView.OnItemSelectedListener {
     private lateinit var binding: ActivityMainBinding
     private lateinit var bottomNavView: BottomNavigationView
 
@@ -23,5 +27,19 @@ class MainActivity : AppCompatActivity() {
         bottomNavView = binding.bottomNavView
         bottomNavView.background = null
         bottomNavView.menu.getItem(2).isEnabled = false
+
+        bottomNavView.setOnItemSelectedListener(this)
+    }
+
+    override fun onNavigationItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            R.id.mProfile -> {
+                findNavController(R.id.navHostFragment).navigate(R.id.loginF)
+            }
+            R.id.mHome -> {
+                findNavController(R.id.navHostFragment).navigate(R.id.postsF)
+            }
+        }
+        return true
     }
 }
